846ef8c7bf469cbfe52eef55b0631821b2edd751
Mateo Gonzalez <matteu20@hotmail.com>
2020-08-17 11:40:12 -0500

    third part

--- src/App.js
+++ src/App.js
@@ -4,31 +4,70 @@ import './App.css';
 import { TodoList } from './TodoList';
 import './TodoList.js';
 
-function App() {
-
-    const todos = [{ text: "Learn React", priority: 5, dueDate: new Date() },
-    { text: "Learn about APIs", priority: 4, dueDate: new Date(2020, 1, 23) },
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        const todos = [{ text: "Learn React", priority: 5, dueDate: new Date() },
+        { text: "Learn about APIs", priority: 4, dueDate: new Date(2020, 1, 23) },
         { text: "write TODO App", priority: 3, dueDate: new Date(2020, 1, 30) }];
+        this.state = { items: todos, aText: '', aPriority: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-                  &lt;h1&gt;TODO React App&lt;/h1&gt;
-        </p>
-        <TodoList todos={todos} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            <div>
+                <h3>TODO</h3>
+                <TodoList todos={this.state.items} />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-todo">
+                        What needs to be done?
+          </label>
+                    <p />
+                    <input
+                        name="aText"
+                        onChange={this.handleChange}
+                        value={this.state.aText}
+                    />
+                    <input
+                        name="aPriority"
+                        onChange={this.handleChange}
+                        value={this.state.aPriority}
+                    />
+
+                    <button>
+                        Add #{this.state.items.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.setState({ [e.target.name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.aText.length || !this.state.aPriority.length) {
+            return;
+        }
+        const newItem = {
+            text: this.state.aText,
+            dueDate: new Date(Date.now()),
+            priority: this.state.aPriority
+        };
+        this.setState(prevState => ({
+            items: prevState.items.concat(newItem),
+            aPriority: '',
+
+            aText: ''
+            
+        }));
+
+    }
 }
 
 export default App;
